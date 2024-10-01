41548a9f7874c38dc532737f43fd5045b1b66c27
rezayas <reza.yaesoubi@yale.edu>
2024-10-01 10:46:32 -0400

    ci checked

--- ci_wta.py
+++ ci_wta.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def calculate_wta_and_simulate(
-        attribute_mean, attribute_std_dev,
-        infection_mean, infection_std_dev,
+        attribute_mean, attribute_std_err,
+        infection_mean, infection_std_err,
         num_draws = 1000, alpha=0.05):
 
     # Step 1: Calculate WTA (or WTP) as the ratio of means
@@ -12,11 +12,11 @@ def calculate_wta_and_simulate(
     wtp_differences = []
     # Repeat the process N times
     for i in range(num_draws):
-        # Draw 1000 samples from N(attribute_mean, attribute_std_dev)
-        attribute_samples = np.random.normal(attribute_mean, attribute_std_dev, 1)
+        # Draw 1000 samples from N(attribute_mean, attribute_std_err)
+        attribute_samples = np.random.normal(attribute_mean, attribute_std_err, 1)
         X_star_n = np.mean(attribute_samples)  # Take the average of the samples
-        # Draw 1000 samples from N(infection_mean, infection_std_dev)
-        infection_samples = np.random.normal(infection_mean, infection_std_dev, 1)
+        # Draw 1000 samples from N(infection_mean, infection_std_err)
+        infection_samples = np.random.normal(infection_mean, infection_std_err, 1)
         Y_star_n = np.mean(infection_samples)  # Take the average of the samples
         # Calculate the difference: X_star_n / Y_star_n - hat_W
         d_star_n = (X_star_n / Y_star_n) - hat_W
@@ -29,11 +29,15 @@ def calculate_wta_and_simulate(
     upper_bound = hat_W - delta_1_minus_alpha_2
     return hat_W, lower_bound, upper_bound, (upper_bound-lower_bound)/2
 
+# estimate, st_err
+n_infected = [-0.309129471,	0.013382487]
+business_closures_3 = [-0.188469312, 0.035301906]
 
-print('expected', -0.1 / 0.3)
+
+print('expected', business_closures_3[0] / n_infected[0])
 results = calculate_wta_and_simulate(
-    attribute_mean=0.1, attribute_std_dev=0.075/2,
-    infection_mean=-0.3, infection_std_dev=0.025/2,
+    attribute_mean=business_closures_3[0], attribute_std_err=business_closures_3[1],
+    infection_mean=n_infected[0], infection_std_err=n_infected[1],
     num_draws=10000, alpha=0.05
 )
 print('estimate: {}, lower bound: {}, upper bound: {}, half width: {}'.format(*results)
