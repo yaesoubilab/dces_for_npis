cb77f69f0405b2ebb1d0e731c2d72d5a19c8a3c1
rezayas <reza.yaesoubi@gmail.com>
2025-01-14 16:40:07 -0500

    logistic regression seems working. start to work on the figure

--- do_stats/run_regression.py
+++ do_stats/run_regression.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
 import statsmodels.api as sm
 
 from defenisions import ROOT_DIR
@@ -10,19 +13,36 @@ variable_labels = ['Gender', 'Hispanic', 'Age', 'Income', 'Residence', 'Child', 
 
 
 '''
-News ['Social media (Instagram, Facebook, X (Twitter), TikTok)'
- 'TV (including cable)' 'Print media (newspapers, journals)'
- 'News apps or websites' 'Radio or podcasts'
- 'Do not read/listen/watch the news' 'Other']
-Remote ['No' 'NA (studying, retired, not in paid employment)' 'Yes']
-Education ['1 or more years of college credit, no degree' 'Postgraduate degree'
- 'High school or GED' 'Some college credits' 'Undergraduate degree'
- 'Associate’s degree']
-Pregnant [nan 'No' 'Yes']
-Vehicle ['No' 'Yes']
-
+Race [
+ 'White' 
+ 'Black or African American'
+ 'American Indian or Native American or Alaskan Native'
+ 'Black or African American,White' 
+ 'Self-define' 'Asian/Asian American'
+ 'Asian/Asian American,White' 
+ 'Prefer not to answer' 
+ 'Chinese,White'
+ 'Native Hawaiian or Pacific Islander,American Indian or Native American or Alaskan Native,White'
+ 'Asian/Asian American,Filipino'
+ 'American Indian or Native American or Alaskan Native,White'
+ 'Middle Eastern or North African,White'
+ 'Native Hawaiian or Pacific Islander'
+ 'Asian/Asian American,Chinese,Filipino' 
+ 'Asian/Asian American,Chinese'
+ 'Japanese' 
+ 'Asian Indian' 
+ 'Chinese'
+ 'Native Hawaiian or Pacific Islander,Asian/Asian American,White'
+ 'Filipino'
+ 'Black or African American,American Indian or Native American or Alaskan Native'
+ 'Asian/Asian American,Japanese' 
+ 'Asian/Asian American,Korean'
+ 'Middle Eastern or North African'
+ 'Black or African American,American Indian or Native American or Alaskan Native,White'
+ 'Korean,White'
+ 'American Indian or Native American or Alaskan Native,Filipino'
+ 'Asian/Asian American,Asian Indian' 'Japanese,Korean']
 '''
-
 
 y_label = ['Class_1']
 
@@ -36,6 +56,12 @@ data_reduced = data_cleaned[variable_labels + y_label]
 # replace 'self_define', 'non_binary', 'prefer_not_to_answer' with 'Other'
 data_reduced.loc[data_reduced['Gender'].isin(['Self-define', 'Non-binary', 'Prefer not to answer']), 'Gender'] = 'Other'
 data_reduced.loc[data_reduced['Age'].isin(['50-64', '35-49' '25-34' '18-24']), 'Age'] = '<65'
+data_reduced.loc[data_reduced['Pregnant'].isin(['No', 'nan']), 'Pregnant'] = 'No'
+data_reduced.loc[data_reduced['Hispanic'].isin(['No', 'nan']), 'Hispanic'] = 'No'
+data_reduced.loc[data_reduced['Education'].isin(
+    ['Postgraduate degree', 'Undergraduate degree', 'Associate’s degree']), 'Education'] = 'With College Degree'
+data_reduced.loc[data_reduced['Education'].isin([
+    '1 or more years of college credit, no degree', 'High school or GED', 'Some college credits']), 'Education'] = 'Without College Degree'
 
 
 # one-hot encode the categorical variables
@@ -53,9 +79,36 @@ X = data_encoded[['Gender_Male', 'Gender_Other',
                   'Vulnerable_contact_Yes',
                   'Health_Insurance_Yes',
                   'Political_Republican', 'Political_Independent', 'Political_Other',
-                  'Self_employed_Yes',]]
+                  'Self_employed_Yes',
+                  'Remote_Yes', 'Remote_NA (studying, retired, not in paid employment)',
+                  'Pregnant_Yes',
+                  'Vehicle_Yes',
+                  'News_Social media (Instagram, Facebook, X (Twitter), TikTok)',
+                  'News_TV (including cable)',
+                  'News_News apps or websites',
+                  'News_Radio or podcasts',
+                  'News_Do not read/listen/watch the news',
+                  'News_Other',
+                  'Education_With College Degree'
+                  ]]
 
 y = data_encoded[y_label]
+
+# correlation matrix
+correlation_matrix = X.corr()
+correlation_matrix.to_csv('correlation_matrix.csv', index=True)
+
+# get the maximum and minimum correlation
+np.fill_diagonal(correlation_matrix.values, np.nan)
+print('Maximum correlation:', correlation_matrix.max().max())
+print('Minimum correlation:', correlation_matrix.min().min())
+print('')
+
+plt.figure(figsize=(15, 14))
+sns.heatmap(correlation_matrix, annot=False, cmap='coolwarm')
+plt.title("Correlation Matrix Heatmap")
+plt.tight_layout()
+plt.savefig('correlation_matrix.png')
 
 # fit the model
 model = sm.Logit(y, X)
@@ -64,4 +117,4 @@ results = model.fit()
 # Display the model summary (coefficients and confidence intervals)
 print(results.summary())
 # export results to csv
-results.summary2().tables[1].to_csv('test.csv')
+results.summary2().tables[1].to_csv('logistic.csv')
