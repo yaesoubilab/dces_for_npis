a0929b5c937f800954831645773dcf933cd9e07d
rezayas <reza.yaesoubi@gmail.com>
2025-01-14 19:17:45 -0500

    logistic regression with fig

--- do_stats/run_regression.py
+++ do_stats/run_regression.py
@@ -118,3 +118,35 @@ results = model.fit()
 print(results.summary())
 # export results to csv
 results.summary2().tables[1].to_csv('logistic.csv')
+
+# visualize
+df = pd.read_csv('logistic.csv')
+df = df.iloc[::-1]
+
+# Filter for estimates and confidence intervals
+df = df[['Unnamed: 0', 'Coef.', '[0.025', '0.975]']]
+df.columns = ['Variables','Estimate', 'CI_Lower', 'CI_Upper']
+
+# Plot estimates with confidence intervals
+fig, ax = plt.subplots(1,1, figsize=(8, 6))
+ax.errorbar(
+    x=df['Estimate'],
+    y=range(len(df['Estimate'])),
+    xerr=[df['Estimate'] - df['CI_Lower'], df['CI_Upper'] - df['Estimate']],
+    fmt='o',
+    capsize=4,
+    label='Estimate'
+)
+
+ax.axvline(x=0, color='gray', linestyle='--', linewidth=1)  # Reference line for zero
+# ax.set_title('Logistic Regression Coefficients with Confidence Intervals')
+ax.set_xlabel('Estimate')
+ax.set_yticks(np.arange(len(df['Estimate'])))
+ax.set_yticklabels(df.iloc[:,0].tolist(), fontsize=10)
+
+ax.grid(axis='x', linestyle='--', alpha=0.7)
+# ax.legend()
+fig.tight_layout()
+
+# Save the plot
+fig.savefig('logistic_regression_ci.png', dpi=300, bbox_inches='tight')
