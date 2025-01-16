9a7b7ab7fa2130991a798a08b67dea8d3cf764d2
rezayas <reza.yaesoubi@gmail.com>
2025-01-15 21:26:47 -0500

    working on double panel figure

--- logistic_reg_analysis/plot_logistic_regression_results.py
+++ logistic_reg_analysis/plot_logistic_regression_results.py
@@ -4,5 +4,6 @@ from fig_support import plot_logistic_regression_coeffs
 if __name__ == '__main__':
 
     # plot
-    plot_logistic_regression_coeffs(
-        fig_size=(6, 11))
+    plot_logistic_regression_coeffs(fig_size=(6, 11), vaccine_scenario='no_vaccine')
+    plot_logistic_regression_coeffs(fig_size=(6, 11), vaccine_scenario='vaccine')
+
