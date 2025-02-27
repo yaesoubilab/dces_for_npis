1abd6ce615e6c9fe5f51aadf0c0bca602d66dda9
rezayas <reza.yaesoubi@gmail.com>
2025-02-27 18:36:24 -0500

    LCA3

--- lca.py
+++ lca.py
@@ -16,6 +16,20 @@ def do_lca_fig():
             distance_between_bars=0.2,
             fig_size=FIG_SIZE)
 
+def do_lca3_fig():
+
+    for estimate_type in ['coeff', 'wta']:
+        do_fig_by_group(
+            estimate_type=estimate_type,
+            group_name='LCA3',
+            group_categories=['class0', 'class1', 'class2'],
+            legend_labels=['Infection Cautious', 'Infection Averse', 'Not sure'],
+            group_colors=['#008080', '#B34D4D', '#FFD700'],
+            x_range=COEFF_X_RANGE if estimate_type == 'coeff' else WTA_X_RANGE,
+            distance_between_bars=0.3,
+            fig_size=FIG_SIZE)
+
 
 if __name__ == '__main__':
     do_lca_fig()
+    do_lca3_fig()
